477671cefcf633e9c538e8dc9e2223f54631151b
NERI00 <nerilichi9@gmail.com>
2021-12-02 20:26:59 +0200

    promis add

--- 02-12.js
+++ 02-12.js
@@ -1,41 +1,34 @@
-const todoTableBody = document.getElementById("todo-table-body")
-
-console.log(todoTableBody)
 const requestAllTodos = () => {
     const ajax = new XMLHttpRequest()
 
-    //1. what to do with the data
     ajax.onreadystatechange = () => {
-        console.log(ajax.readyState)
         let state = ajax.readyState
-        let status = 0
-        // state 4 is compleate
         if (state === 4) {
-            // status 200 is OK
             if (ajax.status === 200) {
                 const todoList = JSON.parse(ajax.responseText)
-                let tableRows = ''
+                displayToDoListInTable(todoList)
+            }
+        }
+    }
+    
+    ajax.open("GET", "https://jsonplaceholder.typicode.com/todos")
+    ajax.send()
 
-                for (let todoElement of todoList) {
-                    if (todoElement.id === 1) {
-                        tableRows += `
+}
+const displayToDoListInTable = (todoList) => {
+    const todoTableBody = document.getElementById("todo-table-body")
+    let tableRows = ''
+    for (let todoElement of todoList) {
+
+        if (todoElement.id === 1) {
+            tableRows += `
                     <tr>
                         <td>${todoElement.title}</td>
                         <td>${todoElement.userId}</td>
                         <td>${todoElement.completed}</td>
                     </tr>
                     `
-
-                    }
-                }
-                todoTableBody.innerHTML = tableRows
-            }
-
         }
     }
-    //2. the adress (url)
-    ajax.open("GET", "https://jsonplaceholder.typicode.com/todos")
-    //3. send the request
-    ajax.send()
-
+    todoTableBody.innerHTML = tableRows
 }
