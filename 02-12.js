3c367a080f58f22fd68080bc67d5dd02281f0d69
NERI00 <nerilichi9@gmail.com>
2021-12-02 20:18:38 +0200

    ajax add

--- 02-12.js
+++ 02-12.js
@@ -1,3 +1,6 @@
+const todoTableBody = document.getElementById("todo-table-body")
+
+console.log(todoTableBody)
 const requestAllTodos = () => {
     const ajax = new XMLHttpRequest()
 
@@ -7,18 +10,31 @@ const requestAllTodos = () => {
         let state = ajax.readyState
         let status = 0
         // state 4 is compleate
-        if(state === 4){
+        if (state === 4) {
             // status 200 is OK
-            if(ajax.status === 200){
-               const todoList = JSON.parse(ajax.responseText)
+            if (ajax.status === 200) {
+                const todoList = JSON.parse(ajax.responseText)
+                let tableRows = ''
 
-               console.log(todoList)
+                for (let todoElement of todoList) {
+                    if (todoElement.id === 1) {
+                        tableRows += `
+                    <tr>
+                        <td>${todoElement.title}</td>
+                        <td>${todoElement.userId}</td>
+                        <td>${todoElement.completed}</td>
+                    </tr>
+                    `
+
+                    }
+                }
+                todoTableBody.innerHTML = tableRows
             }
-            
+
         }
     }
     //2. the adress (url)
-    ajax.open("GET","https://jsonplaceholder.typicode.com/todos")
+    ajax.open("GET", "https://jsonplaceholder.typicode.com/todos")
     //3. send the request
     ajax.send()
 
